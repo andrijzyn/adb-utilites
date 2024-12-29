b785f787ecc5736db47c97e1316168f75b96e66e
andrijzyn <62100293+andrijzyn@users.noreply.github.com>
2024-12-29 04:36:29 +0200

    Generate all needed for Clang-tidy & test
    cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON

---
+++ CMakeFiles/cleaner.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cleaner.dir/link.d"
+  "CMakeFiles/cleaner.dir/main.cpp.o"
+  "CMakeFiles/cleaner.dir/main.cpp.o.d"
+  "cleaner"
+  "cleaner.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cleaner.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/cleaner.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for cleaner.
+# This may be replaced when dependencies are built.
