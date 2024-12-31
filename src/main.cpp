2e729fa585acfb8c9528c2121cc54d36174b7248
andrijzyn <62100293+andrijzyn@users.noreply.github.com>
2024-12-31 20:24:45 +0200

    Added more comments & packages

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,6 @@
 #include "spdlog/spdlog.h"
 #include <array>
-#include <cstdint>
 #include <cstdio>
-#include <iostream>
 #include <list>
 #include <memory>
 #include <stdexcept>
@@ -23,7 +21,7 @@ std::string exec(const std::string &cmd) {
     // Open process using popen and ensure proper closure with unique_ptr
     const std::unique_ptr<FILE, int (*)(FILE *)> pipe(popen(cmd.c_str(), "r"), pclose);
     if (!pipe) {
-        throw std::runtime_error("popen failed!"); // Throw an error if popen fails
+        throw std::runtime_error("Command execution failed!"); // Throw an error if popen fails
     }
 
     // Read data from the pipe and accumulate it into the result string
@@ -40,11 +38,18 @@ void removeRequest(const std::list<std::string> &REQUESTED_PACKAGES) {
         std::string command = "adb shell pm uninstall --user 0 " + package;
         std::string response = exec(command);
 
+        // If package successfully has been removed
         if (response.find(SUCCESS_REMOVED) != std::string::npos) {
             spdlog::info("Package {} has been removed successfully.", package);
-        } else if (response.find(SUCCESS_LACK) != std::string::npos) {
+        }
+
+        // If package hasn't been removed
+        else if (response.find(SUCCESS_LACK) != std::string::npos) {
             spdlog::warn("Package {} is not present.", package);
-        } else {
+        }
+
+        // If all goes to ass
+        else {
             spdlog::critical("Unexpected response for package {}: {}", package, response);
         }
     }
@@ -54,12 +59,11 @@ int main() {
     const std::list<std::string> LIST_PACKAGES = {
         "com.samsung.android.smartswitchassistant",  // Samsung Smart Switch Assistant
         "com.samsung.android.themestore",            // Samsung Theme Store
-        "com.samsung.android.themecenter",           // Samsung Theme Center
         "com.samsung.android.game.gos",              // Game Optimization Service (GOS) | ID play on phone(
         "com.samsung.android.game.gametools",        // Game Tools
         "com.samsung.android.game.gamehome",         // Game Launcher
         "com.samsung.android.kidsinstaller",         // Samsung Kids Installer
-        // "com.samsung.android.aircommandmanager",     // Air Command (S Pen menu) | S Pen doesnt support on A21S
+        // "com.samsung.android.aircommandmanager",  // Air Command (S Pen menu) | S Pen doesnt support on A21S
         "com.samsung.android.app.appsedge",          // Apps Edge
         "com.samsung.android.app.updatecenter",      // Update Center | Support was ending
         "com.samsung.android.shortcutbackupservice", // Shortcut Backup Service
@@ -69,6 +73,10 @@ int main() {
         "com.samsung.android.messaging",             // Samsung Messages | >
         "com.samsung.android.app.contacts",          // Samsung Contacts | >
         "com.samsung.sree",                          // Promoting trash
+        "com.samsung.android.app.tips",
+        "com.samsung.android.app.parentalcare",
+        "com.google.android.tts",
+        "com.samsung.android.aremoji",
         "com.sec.android.app.samsungapps",           // Galaxy Store
         "com.sec.android.easyMover.Agent",           // Smart Switch Agent
         "com.sec.android.app.chromecustomizations",  // Chrome Customizations
