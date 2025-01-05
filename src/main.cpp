d27de6a3a7a7f6663fc60b896bc97650f3a0dba1
andrijzyn <62100293+andrijzyn@users.noreply.github.com>
2025-01-05 07:24:50 +0200

    Separated function menu, added more points
    TODO: fix outputs

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <cstdio>
 #include <curses.h>
 #include <iostream>
+#include <memory>
 #include <optional>
 #include <spdlog/spdlog.h>
 #include <sstream>
@@ -9,13 +10,13 @@
 #include <vector>
 
 // Constants zone
-constexpr size_t MAX_RETURN_SIZE = 128;
+constexpr size_t MAX_RETURN_SIZE = 1024;
 const std::string SUCCESS_REMOVED = "Success";
 const std::string SUCCESS_LACK = "Failure [not installed for 0]";
 const std::string DEVICES_LACK = "adb: no devices/emulators found";
 
 // Enum to represent menu options with a smaller base type (std::uint8_t)
-enum class MenuOption : std::uint8_t { GREET = 0, CLEAN, EXIT };
+enum class MenuOption : uint8_t { GUIDE, LISTPKG, CLEAN, EXIT };
 
 // Package list
 const std::vector<std::string> PACKAGES = {
@@ -59,11 +60,13 @@ const std::vector<std::string> PACKAGES = {
 
 // Execute a command and get its output
 inline std::optional<std::string> exec(const std::string &cmd) {
-    std::array<char, 1024> buffer{};
+    std::array<char, MAX_RETURN_SIZE> buffer{};
     std::string result;
 
     const std::unique_ptr<FILE, int (*)(FILE *)> pipe(popen(cmd.c_str(), "r"), pclose);
-    if (!pipe) { return std::nullopt; }
+    if (!pipe) {
+        return std::nullopt;
+    }
 
     // Read data from the pipe and accumulate it into the result string
     while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
@@ -73,12 +76,20 @@ inline std::optional<std::string> exec(const std::string &cmd) {
     return result;
 }
 
+inline void waitForKeyPress(const std::string &message = "Press any key to return to menu...") {
+    mvprintw(4, 2, "%s", message.c_str());
+    getch();
+    clear();
+}
+
 // Check if adb devices are available
 inline bool checkDevicesAvailable() {
     std::optional<std::string> responseOpt = exec("adb devices");
 
     // Verify the command execution result
-    if (!responseOpt) { return false; }
+    if (!responseOpt) {
+        return false;
+    }
 
     const std::string &response = *responseOpt;
     std::istringstream responseStream(response);
@@ -86,7 +97,11 @@ inline bool checkDevicesAvailable() {
     int lineCount = 0;
 
     // Count lines in response
-    while (std::getline(responseStream, line)) { if (!line.empty()) { ++lineCount; } }
+    while (std::getline(responseStream, line)) {
+        if (!line.empty()) {
+            ++lineCount;
+        }
+    }
 
     // If less than two lines, the device is not connected
     if (lineCount < 2) { // The first line is the heading
@@ -98,30 +113,58 @@ inline bool checkDevicesAvailable() {
     return true; // Devices are available
 }
 
-void removeRequest() {
-    // First, check if devices are available
-    if (!checkDevicesAvailable()) {
-        return; // Exit the function if no devices are found
-    }
+inline void guideBook() {
+    mvprintw(1, 2, "How use? (press ENTER to continue)");
+    getch();
+    mvprintw(2, 2, "Firstly make sure that your computer has installed ADB (Android Debug Bridge)");
+    getch();
+    mvprintw(2, 2, "In smartphone go to Developer mode by pressing 7 times on the build number in 'About device'");
+    getch();
+    clear();
+    mvprintw(1, 2, "How use? (press ENTER to continue)");
+    mvprintw(2, 2, "Press Enable debugging via USB");
+    getch();
+    mvprintw(2, 2, "Connect your Android device to your computer using USB cable");
+    getch();
+}
 
-    // Proceed with package removal if devices are found
+// Proceed with package removal if devices are found
+inline void removeRequest() {
     for (const std::string &package : PACKAGES) {
-        std::string response = exec("adb shell pm uninstall --user 0 " + package);
+        // Execute the adb uninstall command
+        auto responseOpt = exec("adb shell pm uninstall --user 0 " + package);
 
-        // If package successfully has been removed
-        if (response.find(SUCCESS_REMOVED) != std::string::npos) {
-            spdlog::info("Package {} has been removed successfully.", package);
-        }
-        // If package hasn't been removed
-        else if (response.find(SUCCESS_LACK) != std::string::npos) {
-            spdlog::warn("Package {} is not present.", package);
-        }
-        // Unexpected response
-        else {
-            spdlog::critical("Unexpected response for package {}: {}", package, response);
+        // Check if we received a valid response
+        if (responseOpt) {
+            const std::string &response = *responseOpt;
+
+            // If package successfully has been removed
+            if (response.find(SUCCESS_REMOVED) != std::string::npos) {
+                spdlog::info("Package {} has been removed successfully.", package);
+            }
+            // If package hasn't been removed
+            else if (response.find(SUCCESS_LACK) != std::string::npos) {
+                spdlog::warn("Package {} is not present.", package);
+            }
+            // Unexpected response
+            else {
+                spdlog::critical("Unexpected response for package {}: {}", package, response);
+            }
+        } else {
+            spdlog::error("Failed to execute adb command for package: {}", package);
         }
     }
 }
+
+inline void listPackages() {
+    auto packages = exec("adb shell pm list packages");
+    if (packages) {
+        spdlog::info("Packages found:\n{}", *packages);
+    } else {
+        spdlog::error("Failed to retrieve package list.");
+    }
+}
+
 
 // Function to display the menu and get user choice
 MenuOption displayMenu(const std::vector<std::string> &options) {
@@ -136,7 +179,7 @@ MenuOption displayMenu(const std::vector<std::string> &options) {
             if (i == highlight) {
                 attron(A_REVERSE);
             } // Highlight selected option
-            mvprintw(i + 1, 2, options[i].c_str());
+            mvprintw(i + 1, 2, "%s", options[i].c_str());
             if (i == highlight) {
                 attroff(A_REVERSE);
             }
@@ -162,14 +205,32 @@ MenuOption displayMenu(const std::vector<std::string> &options) {
     return static_cast<MenuOption>(choice);
 }
 
-// Function to display a greeting
-void showGreeting() {
-    mvprintw(1, 2, "Hello, World!");
-}
+// Define functional points in menu + MenuOptions
+inline bool menuLoop(const std::vector<std::string> &options) {
+    const MenuOption choice = displayMenu(options);
+    clear(); // Clear screen after option selection
 
-void waitForUserInput() {
-    mvprintw(3, 2, "Press any key to return to menu...");
-    getch(); // Wait for user input to return to menu
+    switch (choice) {
+    case MenuOption::GUIDE:
+        guideBook();
+        break;
+    case MenuOption::LISTPKG:
+        if (checkDevicesAvailable()) {
+            listPackages();
+            waitForKeyPress(); // Wait for the user to press a key before returning to the menu
+        }
+        break;
+    case MenuOption::CLEAN:
+        if (checkDevicesAvailable()) {
+            removeRequest();
+            waitForKeyPress(); // Wait for the user to press a key before returning to the menu
+        }
+        break;
+    case MenuOption::EXIT:
+        return false;
+    }
+
+    return true;
 }
 
 
@@ -183,34 +244,13 @@ int main() {
     keypad(stdscr, true); // Enable arrow keys
 
     // Define menu options
-    const std::vector<std::string> options = {"Action 1: Say Hello", "Action 2: Clean", "Exit"};
+    const std::vector<std::string> options = {"Action 1: How use?",
+                                              "Action 2: List packages",
+                                              "Action 3: Clean device",
+                                              "Exit"};
 
-    while (true) {
-        // Display the menu and get user selection
-        MenuOption choice = displayMenu(options);
-
-        clear(); // Clear screen before executing action
-        switch (choice) {
-        case MenuOption::GREET:
-            showGreeting();
-            spdlog::info("Displayed greeting.");
-            waitForUserInput(); // Return to menu after action
-            break;
-        case MenuOption::CLEAN:
-            endwin(); // Exit ncurses mode before removing packages
-            removeRequest();
-            spdlog::info("Press any key to return to menu...");
-            getchar(); // Wait for user input to return to menu
-            initscr(); // Reinitialize ncurses before returning to the menu
-            break;
-        case MenuOption::EXIT:
-            endwin(); // End ncurses session
-            spdlog::info("Exiting program.");
-            return 0;
-        default:
-            break;
-        }
-    }
+    while (menuLoop(options)) {
+    } // Start menu loop
 
     endwin(); // End ncurses session
     return 0;
